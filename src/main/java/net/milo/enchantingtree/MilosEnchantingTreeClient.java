93f6263b11d16382d211586f663142ee1442696e
MiloC37 <49262457+MiloC37@users.noreply.github.com>
2024-07-29 22:15:57 -0400

    Initial commit

---
+++ src/main/java/net/milo/enchantingtree/MilosEnchantingTreeClient.java
@@ -0,0 +1,10 @@
+package net.milo.enchantingtree;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MilosEnchantingTreeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/milo/enchantingtree/MilosEnchantingTreeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.milo.enchantingtree;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MilosEnchantingTreeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
